c75d910299b5cd763875b7d831beb7c5eaeb1b29
ngangavic <ngangavictor10@gmail.com>
2020-02-02 17:01:39 +0300

    loading progress bar

--- app/src/main/java/com/ngangavic/test/webview/WebViewActivity.kt
+++ app/src/main/java/com/ngangavic/test/webview/WebViewActivity.kt
@@ -1,26 +1,59 @@
 package com.ngangavic.test.webview
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import com.ngangavic.test.R
 
 class WebViewActivity : AppCompatActivity() {
     lateinit var webView:WebView
+    lateinit var progressBar:ProgressBar
+
+    companion object{
+        var ShowOrHideWebViewInitialUse = "show"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
         webView=findViewById(R.id.webView)
+        progressBar=findViewById(R.id.progressBar)
         webView.settings.javaScriptEnabled=true
-        webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://www.pu.ac.ke")
+        webView.webViewClient=object:WebViewClient(){
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progressBar.visibility=View.VISIBLE
+                webView.visibility=View.GONE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                ShowOrHideWebViewInitialUse = "hide"
+                progressBar.setVisibility(View.GONE)
+                webView.visibility=View.VISIBLE
+                super.onPageFinished(view, url)
+            }
+        }
+
+        webView.webChromeClient=object :WebChromeClient(){
+
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progressBar.setProgress(newProgress)
+                if(newProgress==100){
+                    progressBar.setVisibility(View.GONE)
+                }
+            }
+        }
+
+
+        webView.loadUrl("https://www.pesapal.com")
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        // Check if the key event was the Back button and if there's history
         if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
             webView.goBack()
             return true
