f7b651a942600195d6649942be5a2799d1648653
ngangavic <ngangavictor10@gmail.com>
2020-02-08 16:15:57 +0300

    using service with volley

--- app/src/main/java/com/ngangavic/test/service/MyService.kt
+++ app/src/main/java/com/ngangavic/test/service/MyService.kt
@@ -4,18 +4,29 @@ import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
+import android.util.Log
 import android.widget.Toast
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
 import com.ngangavic.test.R
+import org.json.JSONObject
+import java.util.HashMap
 
 class MyService : Service() {
 
     lateinit var player: MediaPlayer
+    lateinit var queue: RequestQueue
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
     override fun onCreate() {
+        queue= Volley.newRequestQueue(this)
         Toast.makeText(this, "Service created!", Toast.LENGTH_SHORT).show()
         player = MediaPlayer.create(this, R.raw.laugh)
         player.isLooping = false
@@ -23,11 +34,41 @@ class MyService : Service() {
 
     override fun onStart(intent: Intent?, startId: Int) {
         Toast.makeText(this, "Service started!", Toast.LENGTH_SHORT).show()
+        sendRequest()
         player.start()
     }
 
     override fun onDestroy() {
         Toast.makeText(this, "Service stopped!", Toast.LENGTH_SHORT).show()
         player.stop()
+        queue.cancelAll(this)
+    }
+
+    private fun sendRequest(){
+        val str = object : StringRequest(
+                Method.POST, "https://your url",
+                Response.Listener { response ->
+                    Log.d("SERVICE", response.toString())
+                    val obj = JSONObject(response)
+
+
+                },
+                Response.ErrorListener { error ->
+
+                    error.printStackTrace()
+
+                }) {
+            override fun getParams(): Map<String, String> {
+                val param = HashMap<String, String>()
+                param["uid"] = "0"
+                return param
+            }
+        }
+        str.retryPolicy = DefaultRetryPolicy(
+                7000,
+                5,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+        queue.add(str)
     }
 }
