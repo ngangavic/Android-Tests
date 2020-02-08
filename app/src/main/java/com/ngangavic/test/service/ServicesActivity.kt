2fa2f65d01853678ad6f0307c17ee83f26f69779
ngangavic <ngangavictor10@gmail.com>
2020-02-08 15:37:31 +0300

    service UI

---
+++ app/src/main/java/com/ngangavic/test/service/ServicesActivity.kt
@@ -0,0 +1,13 @@
+package com.ngangavic.test.service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ngangavic.test.R
+
+class ServicesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_services)
+    }
+}
