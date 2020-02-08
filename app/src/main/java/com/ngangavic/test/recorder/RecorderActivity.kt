58559c2544db3d41ce353cb332e5590181741bc6
ngangavic <ngangavictor10@gmail.com>
2020-02-08 10:07:14 +0300

    record activity button onclick listeners

--- app/src/main/java/com/ngangavic/test/recorder/RecorderActivity.kt
+++ app/src/main/java/com/ngangavic/test/recorder/RecorderActivity.kt
@@ -1,13 +1,56 @@
 package com.ngangavic.test.recorder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.ngangavic.test.R
 
 class RecorderActivity : AppCompatActivity() {
+    lateinit var buttonStart: Button
+    lateinit var buttonPause: Button
+    lateinit var buttonStop: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recorder)
+        buttonStart = findViewById(R.id.buttonStart)
+        buttonStop = findViewById(R.id.buttonStop)
+        buttonPause = findViewById(R.id.buttonPause)
+
+        buttonStart.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                ActivityCompat.requestPermissions(this, permissions, 0)
+            } else {
+                startRecording()
+            }
+        }
+
+        buttonPause.setOnClickListener {
+            pauseRecording()
+        }
+
+        buttonStop.setOnClickListener {
+            stopRecording()
+        }
+
+    }
+
+    private fun stopRecording() {
+
+    }
+
+    private fun pauseRecording() {
+
+    }
+
+    private fun startRecording() {
+
     }
 }
