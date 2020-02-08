545bf6eee930311e03640cc2796073879cb2aba0
ngangavic <ngangavictor10@gmail.com>
2020-02-08 15:57:18 +0300

    service setup

--- app/src/main/java/com/ngangavic/test/service/ServicesActivity.kt
+++ app/src/main/java/com/ngangavic/test/service/ServicesActivity.kt
@@ -1,13 +1,33 @@
 package com.ngangavic.test.service
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.ngangavic.test.R
 
 class ServicesActivity : AppCompatActivity() {
+    lateinit var buttonStart: Button
+    lateinit var buttonStop: Button
+    lateinit var serviceLayout: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_services)
+        buttonStart = findViewById(R.id.buttonStart)
+        buttonStop = findViewById(R.id.buttonStop)
+        serviceLayout = findViewById(R.id.serviceLayout)
+
+        buttonStart.setOnClickListener {
+            startService(Intent(this, MyService::class.java))
+        }
+
+        buttonStop.setOnClickListener {
+            stopService(Intent(this, MyService::class.java))
+        }
+
     }
+
+
 }
