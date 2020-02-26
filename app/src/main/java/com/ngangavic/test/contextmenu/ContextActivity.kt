45d7eb9e1979846b6892468868973e811567693e
ngangavic <ngangavictor10@gmail.com>
2020-02-26 11:50:43 +0300

    Beautiful Context Menu

---
+++ app/src/main/java/com/ngangavic/test/contextmenu/ContextActivity.kt
@@ -0,0 +1,13 @@
+package com.ngangavic.test.contextmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ngangavic.test.R
+
+class ContextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_context)
+    }
+}
