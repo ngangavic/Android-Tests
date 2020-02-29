cad42861de712253e75e3b0b923e964c31adb39e
ngangavic <ngangavictor10@gmail.com>
2020-02-29 17:21:36 +0300

    simple notification

--- app/src/main/java/com/ngangavic/test/notifications/NotificationActivity.kt
+++ app/src/main/java/com/ngangavic/test/notifications/NotificationActivity.kt
@@ -7,14 +7,25 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.ngangavic.test.R
 
 class NotificationActivity : AppCompatActivity() {
+
+    lateinit var buttonSimpleNotification:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
         createNotificationChannel()
+
+        buttonSimpleNotification=findViewById(R.id.buttonSimpleNotification)
+
+        buttonSimpleNotification.setOnClickListener {
+            simpleNotification()
+        }
     }
 
     private fun createNotificationChannel() {
@@ -32,6 +43,17 @@ class NotificationActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(channel)
         }
         Log.d("NOTIFICATION","Notification channel created")
+    }
+
+    private fun simpleNotification(){
+        var builder = NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("Notification")
+                .setContentText("This is a simple notification.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(1000, builder.build())
+        }
     }
 
 
