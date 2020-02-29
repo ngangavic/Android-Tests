3e34dfeefdc24918da4134ec4dec193665210a38
ngangavic <ngangavictor10@gmail.com>
2020-02-29 17:09:19 +0300

    notification channel

--- app/src/main/java/com/ngangavic/test/notifications/NotificationActivity.kt
+++ app/src/main/java/com/ngangavic/test/notifications/NotificationActivity.kt
@@ -1,7 +1,12 @@
 package com.ngangavic.test.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.ngangavic.test.R
 
 class NotificationActivity : AppCompatActivity() {
@@ -9,5 +14,26 @@ class NotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+        createNotificationChannel()
     }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channelId="1"
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+        Log.d("NOTIFICATION","Notification channel created")
+    }
+
+
+
 }
