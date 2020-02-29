5a7f5735487acb9963b2644113d9bf67940bb561
ngangavic <ngangavictor10@gmail.com>
2020-02-29 21:25:13 +0300

    expandable notification

--- app/src/main/java/com/ngangavic/test/notifications/NotificationActivity.kt
+++ app/src/main/java/com/ngangavic/test/notifications/NotificationActivity.kt
@@ -14,17 +14,36 @@ import com.ngangavic.test.R
 
 class NotificationActivity : AppCompatActivity() {
 
-    lateinit var buttonSimpleNotification:Button
+    lateinit var buttonSimpleNotification: Button
+    lateinit var buttonExpandableNotification: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
         createNotificationChannel()
 
-        buttonSimpleNotification=findViewById(R.id.buttonSimpleNotification)
+        buttonSimpleNotification = findViewById(R.id.buttonSimpleNotification)
+        buttonExpandableNotification = findViewById(R.id.buttonExpandableNotification)
 
         buttonSimpleNotification.setOnClickListener {
             simpleNotification()
+        }
+
+        buttonExpandableNotification.setOnClickListener {
+            createExpandableNotification()
+        }
+    }
+
+    private fun createExpandableNotification() {
+        var builder = NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("Notification")
+                .setContentText("Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook.")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(this)) {
+            notify(1000, builder.build())
         }
     }
 
@@ -33,7 +52,7 @@ class NotificationActivity : AppCompatActivity() {
             val name = getString(R.string.channel_name)
             val descriptionText = getString(R.string.channel_description)
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channelId="1"
+            val channelId = "1"
             val channel = NotificationChannel(channelId, name, importance).apply {
                 description = descriptionText
             }
@@ -42,20 +61,19 @@ class NotificationActivity : AppCompatActivity() {
                     getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
-        Log.d("NOTIFICATION","Notification channel created")
+        Log.d("NOTIFICATION", "Notification channel created")
     }
 
-    private fun simpleNotification(){
+    private fun simpleNotification() {
         var builder = NotificationCompat.Builder(this, "1")
                 .setSmallIcon(R.drawable.logo)
                 .setContentTitle("Notification")
-                .setContentText("This is a simple notification.")
+                .setContentText("This is a simple notification. Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook.")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
         with(NotificationManagerCompat.from(this)) {
             notify(1000, builder.build())
         }
     }
-
 
 
 }
