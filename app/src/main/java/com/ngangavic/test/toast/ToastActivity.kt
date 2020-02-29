2e1b43d8da95442c66968071504387ad09579fe4
james-muriithi <muriithijames556@gmail.com>
2020-02-29 15:30:40 +0300

    Toast Activity

---
+++ app/src/main/java/com/ngangavic/test/toast/ToastActivity.kt
@@ -0,0 +1,13 @@
+package com.ngangavic.test.toast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ngangavic.test.R
+
+class ToastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toast)
+    }
+}
