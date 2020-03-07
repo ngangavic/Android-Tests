06fdd279b16411d55c893c746dc2ececde28cfbb
ngangavic <ngangavictor10@gmail.com>
2020-03-07 13:31:32 +0300

    job scheduler acitivty

---
+++ app/src/main/java/com/ngangavic/test/jobscheduler/JobSchedulerActivity.kt
@@ -0,0 +1,13 @@
+package com.ngangavic.test.jobscheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ngangavic.test.R
+
+class JobSchedulerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_scheduler)
+    }
+}
