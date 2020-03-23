330316348be8fdcf9cb98d326340db8df29f9e5b
ngangavic <ngangavictor10@gmail.com>
2020-03-23 22:48:04 +0300

    google admob test complete

--- app/src/main/java/com/ngangavic/test/googleadmob/GoogleAdMobActivity.kt
+++ app/src/main/java/com/ngangavic/test/googleadmob/GoogleAdMobActivity.kt
@@ -2,12 +2,21 @@ package com.ngangavic.test.googleadmob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.ngangavic.test.R
 
 class GoogleAdMobActivity : AppCompatActivity() {
 
+    lateinit var adView:AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_ad_mob)
+        MobileAds.initialize(this) {}
+        adView=findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 }
