9aa49a6dc1b0e27cfcba6c1f5fc5fb62e41036ee
ngangavic <ngangavictor10@gmail.com>
2020-03-26 11:01:33 +0300

    fcm set up

---
+++ app/src/main/java/com/ngangavic/test/fcm/CloudMessagingActivity.kt
@@ -0,0 +1,13 @@
+package com.ngangavic.test.fcm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ngangavic.test.R
+
+class CloudMessagingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cloud_messaging)
+    }
+}
