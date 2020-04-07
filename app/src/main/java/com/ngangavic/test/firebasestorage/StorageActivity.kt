0e4a46b73e964f43cbaceb20f2cb4bf59e7fcefc
ngangavic <ngangavictor10@gmail.com>
2020-04-07 23:07:00 +0300

    image upload from camera

--- app/src/main/java/com/ngangavic/test/firebasestorage/StorageActivity.kt
+++ app/src/main/java/com/ngangavic/test/firebasestorage/StorageActivity.kt
@@ -1,14 +1,21 @@
 package com.ngangavic.test.firebasestorage
 
+import android.Manifest
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
@@ -27,6 +34,7 @@ class StorageActivity : AppCompatActivity() {
     lateinit var buttonUpload: Button
     lateinit var buttonViewImages: Button
     lateinit var buttonPickImage: Button
+    lateinit var buttonCamera: Button
     private lateinit var auth: FirebaseAuth
 
 
@@ -37,6 +45,7 @@ class StorageActivity : AppCompatActivity() {
         buttonUpload = findViewById(R.id.buttonUpload)
         buttonViewImages = findViewById(R.id.buttonViewImages)
         buttonPickImage = findViewById(R.id.buttonPickImage)
+        buttonCamera = findViewById(R.id.buttonCamera)
         storage = Firebase.storage
         auth = FirebaseAuth.getInstance()
 
@@ -52,11 +61,22 @@ class StorageActivity : AppCompatActivity() {
         buttonPickImage.setOnClickListener { chooseImage() }
 
         buttonUpload.setOnClickListener { uploadImage() }
+
+        buttonCamera.setOnClickListener { useCamera() }
+
+        requestPermissions()
     }
 
     private fun chooseImage() {
         val intent = Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT)
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), 200)
+    }
+
+    private fun useCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(applicationContext.getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 201)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -69,6 +89,10 @@ class StorageActivity : AppCompatActivity() {
             } catch (e: IOException) {
                 e.printStackTrace()
             }
+        } else if (requestCode == 201 && resultCode == RESULT_OK) {
+            val extras = data!!.extras
+            val imageBitmap = extras!!["data"] as Bitmap?
+            imageView.setImageBitmap(imageBitmap)
         }
     }
 
@@ -77,9 +101,9 @@ class StorageActivity : AppCompatActivity() {
         imageView.isDrawingCacheEnabled = true
         imageView.buildDrawingCache()
         val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos)
-        val data = baos.toByteArray()
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
+        val data = byteArrayOutputStream.toByteArray()
 
         var uploadTask = storageRef.putBytes(data)
         uploadTask.addOnFailureListener {
@@ -87,5 +111,83 @@ class StorageActivity : AppCompatActivity() {
         }.addOnSuccessListener {
             Toast.makeText(applicationContext, "Success", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            300 -> {
+                val perms = HashMap<String, Int>()
+                perms[Manifest.permission.CAMERA] = PackageManager.PERMISSION_GRANTED
+
+                if (grantResults.size > 0) {
+                    for (i in permissions.indices)
+                        perms[permissions[i]] = grantResults[i]
+
+                    if (perms[Manifest.permission.CAMERA] == PackageManager.PERMISSION_GRANTED
+                            && perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED
+                            && perms[Manifest.permission.READ_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED
+                    ) {
+                    } else {
+
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                            showDialogOK("Service Permissions are required for this app",
+                                    DialogInterface.OnClickListener { dialog, which ->
+                                        when (which) {
+                                            DialogInterface.BUTTON_POSITIVE -> requestPermissions()
+                                            DialogInterface.BUTTON_NEGATIVE ->
+                                                finish()
+                                        }
+                                    })
+                        } else {
+                            explain("You need to give some mandatory permissions to continue. Do you want to go to app settings?")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun requestPermissions(): Boolean {
+        val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+
+        val listPermissionsNeeded = ArrayList<String>()
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA)
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    listPermissionsNeeded.toTypedArray(),
+                    300
+            )
+            return false
+        }
+        return true
+    }
+
+    private fun showDialogOK(message: String, okListener: DialogInterface.OnClickListener) {
+        AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", okListener)
+                .create()
+                .show()
+    }
+
+    private fun explain(msg: String) {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setMessage(msg)
+                .setPositiveButton("Yes") { paramDialogInterface, paramInt ->
+                    startActivity(
+                            Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:com.ngangavic.test")))
+                }
+                .setNegativeButton("Cancel") { paramDialogInterface, paramInt -> finish() }
+        dialog.show()
     }
 }
