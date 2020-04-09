e69d4b62d868db815c3af87caf4392f1a1d7342b
ngangavic <ngangavictor10@gmail.com>
2020-04-09 13:29:18 +0300

    image data class

---
+++ app/src/main/java/com/ngangavic/test/firebasestorage/Image.kt
@@ -0,0 +1,11 @@
+package com.ngangavic.test.firebasestorage
+
+class Image {
+    var name:String?=null
+    var url: String? =null
+
+    constructor(name: String?, url: String?) {
+        this.name = name
+        this.url = url
+    }
+}
