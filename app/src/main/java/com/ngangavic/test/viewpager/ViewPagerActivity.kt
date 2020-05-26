9320144561acbb87d1d3e469be79279d61d237e5
ngangavic <ngangavictor10@gmail.com>
2020-05-26 09:55:36 +0300

    view pager

---
+++ app/src/main/java/com/ngangavic/test/viewpager/ViewPagerActivity.kt
@@ -0,0 +1,43 @@
+package com.ngangavic.test.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.ngangavic.test.R
+
+private const val NUM_PAGES = 5
+
+class ViewPagerActivity : AppCompatActivity() {
+
+    private lateinit var mPager: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager)
+
+        mPager = findViewById(R.id.pager)
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+    }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getCount(): Int = NUM_PAGES
+
+        override fun getItem(position: Int): Fragment = ViewPagerFragment()
+    }
+}
+
