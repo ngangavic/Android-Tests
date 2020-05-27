0b48923a532b6534128cf08b022692d979ea98c1
ngangavic <ngangavictor10@gmail.com>
2020-05-27 15:49:19 +0300

    expandable list view

--- app/src/main/java/com/ngangavic/test/expandablelistview/ExpandableListViewActivity.kt
+++ app/src/main/java/com/ngangavic/test/expandablelistview/ExpandableListViewActivity.kt
@@ -13,9 +13,7 @@ class ExpandableListViewActivity : AppCompatActivity() {
 
     lateinit var expandableListView: ExpandableListView
     lateinit var expandableListAdapter: ExpandableListAdapter
-    lateinit var expandableListSubCounty: List<String>
-
-    //    lateinit var expandableListWard:List<String>
+    lateinit var expandableListSubCounty: MutableList<String>
     private lateinit var expandableListWard: HashMap<String, List<String>>
     lateinit var tasks: List<String>
 
@@ -30,21 +28,20 @@ class ExpandableListViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expandable_list_view)
 
-        expandableListView = findViewById(R.id.expandableListView)
+        expandableListView = findViewById(R.id.expandableListView) as ExpandableListView
         mFirebasedatabase = FirebaseDatabase.getInstance();
         mTasksDatabaseReference = mFirebasedatabase.getReference().child("android-test").child("location")
         Log.d("DATA", mTasksDatabaseReference.toString())
-        expandableListWard = HashMap<String, List<String>>()
+        expandableListWard = HashMap()
         expandableListSubCounty = ArrayList()
         tasks = ArrayList()
-        getData()
-//        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
-//        expandableListDetail = ExpandableListDataPump.getData();
-//        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+//        getData()
+        prepareListData()
+
         expandableListAdapter = ExpandableListAdapter(this, expandableListSubCounty, expandableListWard)
-//        expandableListView.setAdapter(expandableListAdapter);
+
         expandableListView.setAdapter(expandableListAdapter)
-        expandableListAdapter.notifyDataSetChanged()
+//        expandableListAdapter.notifyDataSetChanged()
 
         expandableListView!!.setOnGroupExpandListener { groupPosition ->
             Toast.makeText(
@@ -76,6 +73,60 @@ class ExpandableListViewActivity : AppCompatActivity() {
         }
     }
 
+    private fun prepareListData() {
+        expandableListWard = HashMap()
+        expandableListSubCounty = ArrayList()
+        // Adding child data
+        expandableListSubCounty.add("Kilifi North")
+        expandableListSubCounty.add("Kilifi South")
+        expandableListSubCounty.add("Malindi")
+
+        // Adding child data
+        val kilifiNorth = ArrayList<String>()
+        kilifiNorth.add("KN 1")
+        kilifiNorth.add("KN 2")
+        kilifiNorth.add("KN 3")
+        kilifiNorth.add("KN 4")
+        kilifiNorth.add("KN 5")
+        kilifiNorth.add("KN 6")
+        kilifiNorth.add("KN 7")
+        kilifiNorth.add("KN 8")
+        kilifiNorth.add("KN 9")
+        kilifiNorth.add("KN 10")
+
+        val kilifiSouth = ArrayList<String>()
+        kilifiSouth.add("KS 1")
+        kilifiSouth.add("KS 2")
+        kilifiSouth.add("KS 3")
+        kilifiSouth.add("KS 4")
+        kilifiSouth.add("KS 5")
+        kilifiSouth.add("KS 6")
+        kilifiSouth.add("KS 7")
+        kilifiSouth.add("KS 8")
+        kilifiSouth.add("KS 9")
+        kilifiSouth.add("KS 10")
+
+        val malindi = ArrayList<String>()
+        malindi.add("ML 1")
+        malindi.add("ML 2")
+        malindi.add("ML 3")
+        malindi.add("ML 4")
+        malindi.add("ML 5")
+        malindi.add("ML 6")
+        malindi.add("ML 7")
+        malindi.add("ML 8")
+        malindi.add("ML 9")
+        malindi.add("ML 10")
+
+        expandableListWard[expandableListSubCounty[0]] = kilifiNorth // Header, Child data
+        expandableListWard[expandableListSubCounty[1]] = kilifiSouth
+        expandableListWard[expandableListSubCounty[2]] = malindi
+
+//        expandableListWard.put("kilifi North",kilifiNorth)
+//        expandableListWard.put("kilifi South",kilifiSouth)
+//        expandableListWard.put("Malindi",malindi)
+    }
+
     private fun getData() {
         mTasksDatabaseReference.addChildEventListener(object : ChildEventListener {
             override fun onCancelled(p0: DatabaseError) {}
@@ -98,7 +149,7 @@ class ExpandableListViewActivity : AppCompatActivity() {
 //                p0.key?.let { expandableListWard.put(it, tasks) }
                 expandableListSubCounty.toMutableList().add(p0.key.toString())
 //                p0.key?.let { expandableListSubCounty.toMutableList().add(it) }
-
+                expandableListAdapter.notifyDataSetChanged()
 //                tasks=ArrayList()
 
                 Log.d("DATA", "onChildAdded: dataSnapshot.getChildren: " + p0.getChildren());
