e729c09fc6797f8156813a87679606a4262ccc8c
ngangavic <ngangavictor10@gmail.com>
2020-07-02 00:21:49 +0300

    chat message holder

---
+++ app/src/main/java/com/ngangavic/test/chat/MessageHolder.kt
@@ -0,0 +1,15 @@
+package com.ngangavic.test.chat
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavic.test.R
+
+class MessageHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+    var textViewName: TextView=itemView.findViewById(R.id.textViewName) as TextView
+    var textViewTime: TextView=itemView.findViewById(R.id.textViewTime) as TextView
+    var textViewMessage: TextView=itemView.findViewById(R.id.textViewMessage) as TextView
+
+}
