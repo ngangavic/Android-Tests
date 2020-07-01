af9d9e12dbc11dd4063142067d8a838d27e11085
ngangavic <ngangavictor10@gmail.com>
2020-07-02 00:22:02 +0300

    chat messages adapter

---
+++ app/src/main/java/com/ngangavic/test/chat/MessagesAdapter.kt
@@ -0,0 +1,45 @@
+package com.ngangavic.test.chat
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavic.test.R
+
+class MessagesAdapter(private var context: Context, private val messages: ArrayList<Message>) :
+        RecyclerView.Adapter<MessageHolder>() {
+
+    private val me = 100
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageHolder {
+        val viewHolder: MessageHolder
+        viewHolder = if (viewType == me) {
+            val layoutView =
+                    LayoutInflater.from(parent.context).inflate(R.layout.row_chat_me, parent, false)
+            MessageHolder(layoutView)
+        } else {
+            val layoutView =
+                    LayoutInflater.from(parent.context).inflate(R.layout.row_chat_other, parent, false)
+            MessageHolder(layoutView)
+        }
+
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return messages.size
+    }
+
+    override fun onBindViewHolder(holder: MessageHolder, position: Int) {
+        holder.textViewMessage.text=messages[position].message
+        holder.textViewName.text=messages[position].person
+        holder.textViewTime.text=messages[position].time
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (messages[position].uid == "") {
+            return me
+        }
+        return position
+    }
+}
