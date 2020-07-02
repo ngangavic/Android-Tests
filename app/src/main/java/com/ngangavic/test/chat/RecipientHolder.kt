f3ad9d400c8e3013ad7c1b2324b83caea34c96fa
ngangavic <ngangavictor10@gmail.com>
2020-07-02 15:41:07 +0300

    chat recipient holder

---
+++ app/src/main/java/com/ngangavic/test/chat/RecipientHolder.kt
@@ -0,0 +1,11 @@
+package com.ngangavic.test.chat
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavic.test.R
+
+class RecipientHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+    val textViewName: TextView =itemView.findViewById(R.id.textViewName) as TextView
+}
