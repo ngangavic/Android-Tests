23c41c84e25dc22a38ea9f55ff36001ee8c5ec41
ngangavic <ngangavictor10@gmail.com>
2020-07-25 02:31:18 +0300

    search menu

--- app/src/main/java/com/ngangavic/test/search/SearchActivity.kt
+++ app/src/main/java/com/ngangavic/test/search/SearchActivity.kt
@@ -2,10 +2,23 @@ package com.ngangavic.test.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.ngangavic.test.R
 
 class SearchActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
