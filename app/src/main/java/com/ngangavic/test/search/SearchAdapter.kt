d85cfec815f4ac609530cda55ec5afb6acbe34f6
ngangavic <ngangavictor10@gmail.com>
2020-07-25 14:12:32 +0300

    search adapter

---
+++ app/src/main/java/com/ngangavic/test/search/SearchAdapter.kt
@@ -0,0 +1,29 @@
+package com.ngangavic.test.search
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavic.test.R
+import com.ngangavic.test.chat.Message
+import java.util.ArrayList
+
+class SearchAdapter(private var context: Context, private val search: ArrayList<Search>) :
+        RecyclerView.Adapter<SearchHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchHolder {
+        val viewHolder: SearchHolder?
+        val layoutView =
+                LayoutInflater.from(parent.context).inflate(R.layout.row_search, parent, false)
+        viewHolder = SearchHolder(layoutView)
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+       return search.size
+    }
+
+    override fun onBindViewHolder(holder: SearchHolder, position: Int) {
+        holder.textViewName.text = search[position].name
+
+    }
+}
